80a870169e830952176c2e33042136a0e57e6961
Noela-Murugi <murugin254@gmail.com>
2022-11-27 17:06:49 +0300

    relations update

--- app/models/restraunt_pizza.rb
+++ app/models/restraunt_pizza.rb
@@ -1,5 +1,5 @@
 class RestrauntPizza < ApplicationRecord
-  validates :price , presence: true , numericality: { only_integer: true , greater_than_or_equal_to: 1 , less_than_or_equal_to: 30 }
+  validates :price, numericality: { greater_than: 0, less_than_or_equal_to: 30 }
   belongs_to :pizza
   belongs_to :restraunt
 end
