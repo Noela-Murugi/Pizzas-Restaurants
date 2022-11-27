540c1a5a89e8e5396875dfd315a7e793ab78abd3
Noela-Murugi <murugin254@gmail.com>
2022-11-27 17:03:44 +0300

    seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding database..."
+restaurants  = Restaurant.create(  [
+    { name: "Restaurant1", address: "Embakasi" },
+    { name: "Restaurant2", address: "Langata" },
+    { name: "Restaurant3", address: "Langata" }
+]
+)
+
+pizza  = Pizza.create(  [
+    { name: "Hawaiian", ingredients: "Mushrooms"},
+    { name: "BBQ", ingredients: "Cheese"},
+    { name: "Tikka", ingredients: "Chicken"}
+]
+)
+puts "Done Seeding"
