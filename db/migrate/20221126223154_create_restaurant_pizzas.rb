e15254fe63d0e26777a7b6a6d74824bb023df3db
Noela-Murugi <murugin254@gmail.com>
2022-11-27 16:00:10 +0300

    include references

--- db/migrate/20221126223154_create_restaurant_pizzas.rb
+++ db/migrate/20221126223154_create_restaurant_pizzas.rb
@@ -2,7 +2,8 @@ class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurant_pizzas do |t|
       t.integer :price
-    
+      t.references :pizza, null: false, foreign_key: true
+      t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
     end
