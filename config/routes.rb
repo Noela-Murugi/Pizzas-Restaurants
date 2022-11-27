75d2fa38fa9b1a0a2da25edf5eef348e78e19dee
Noela-Murugi <murugin254@gmail.com>
2022-11-27 15:35:15 +0300

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :restaurant_pizzas, only: [:create]
+  resources :pizzas, only: [:index]
+  resources :restaurants, only: [:index, :show, :destroy] do
+    resources :pizzas, only: [:index]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
